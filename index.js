088d234e9082fedcaf6652554040116259ff9a5e
Nanagabby95 <nancitonana86@gmail.com>
2023-08-26 07:21:43 +0100

    removed logo label

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-  var typingElement = document.getElementById('typing-animation');
-  typingElement.classList.add('typing');
+//   var typingElement = document.getElementById('typing-animation');
+//   typingElement.classList.add('typing');
 
  
 const hamburger = document.querySelector('.hamburger');
@@ -19,6 +19,17 @@ document.querySelectorAll(".nav-link").forEach((n) => {
   });
 });
 
+// ALGORITHM
+// function linearSearch(arr, target) {
+//  for(let i=0; i<arr.length; i++){
+//   if (arr[i]===target){
+//     return i
+//   }
+//  }return -1
+//   } 
+//   console.log(linearSearch([-5,2,10,4,6],10));
+//   console.log(linearSearch([-5,2,10,4,6],6));
+//   console.log(linearSearch([-5,2,10,4,6],20));
 
-  
-  
+
+
